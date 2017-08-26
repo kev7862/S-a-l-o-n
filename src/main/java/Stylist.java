9dade88fbe7b0ce68d6beb98dc0f6921e3197c89
kev7862 <kevmatite7862@gmail.com>
2017-08-26 10:38:36 +0300

    // trying to make a connection into the database stylists table and insert value into the name column.

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -8,5 +8,13 @@ public class Stylist {
 //setting the variable into a class
   public Stylist(String name) {
     this.name=name;
+// trying to make a connection into the database stylists table and insert value into the name column.
+    try(Connection cn = DB.sql2o.open()) {
+    String sql = "INSERT INTO stylists (name) VALUES (:name)";
+    this.id = (int) cn.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .executeUpdate()
+      .getKey();
+  }
   }
 }
