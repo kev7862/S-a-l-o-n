3463c6836f3b1abae299b3a6fa65fc9be16dcbd9
kev7862 <kevmatite7862@gmail.com>
2017-08-27 08:57:01 +0300

    //Trying to Establish a connection to the database procedures table and insert values into the description and price column.

--- src/main/java/Procedure.java
+++ src/main/java/Procedure.java
@@ -9,3 +9,13 @@ public class Procedure {
   public Procedure(String description, float price) {
     this.description=description;
     this.price=price;
+//Trying to Establish a connection to the database procedures table and insert values into the description and price column.
+    try(Connection cn = DB.sql2o.open()) {
+    String sql = "INSERT INTO procedures (description, price) VALUES (:description, :price)";
+    this.id = (int) cn.createQuery(sql, true)
+      .addParameter("description", this.description)
+      .addParameter("price", this.price)
+      .executeUpdate()
+      .getKey();
+  }
+}
