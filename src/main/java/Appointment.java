b907774e724724f61131f7b947b865e4653f7f30
kev7862 <kevmatite7862@gmail.com>
2017-08-27 08:45:00 +0300

    Made a quick edit on my comments in Appointment.java

--- src/main/java/Appointment.java
+++ src/main/java/Appointment.java
@@ -1,19 +1,19 @@
 import org.sql2o.*;
 import java.util.List;
 import java.util.Date;
-
+// Declaring my variables.
 public class Appointment {
   private int id=0;
   private String time;
   private int clientid;
   private int procedureid;
-
+// giving my variables a class and classtype..
   public Appointment (String time, int clientid, int procedureid) {
     this.time = time;
     this.clientid = clientid;
     this.procedureid = procedureid;
 
-// Trying to connect to the database.
+// Trying to make a connection to the database appointments table, and insert values into: time, clientid, procedureid.
     try(Connection cn = DB.sql2o.open()) {
     String sql = "INSERT INTO appointments (time, clientid, procedureid) VALUES (:time, :clientid, :procedureid)";
     this.id = (int) cn.createQuery(sql, true)
