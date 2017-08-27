7a25c1d8b0196458808eb33cb811a9d1d52cc5c3
kev7862 <kevmatite7862@gmail.com>
2017-08-27 11:12:39 +0300

    Added a home route to my navbar in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,3 +11,12 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("appointents", Appointment.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
