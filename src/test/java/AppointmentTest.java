27b3e2c443fbb3a92d4aa27db827675002fd4ce5
kev7862 <kevmatite7862@gmail.com>
2017-08-28 08:10:40 +0300

    Run my first database test which successfully passes.

--- src/test/java/AppointmentTest.java
+++ src/test/java/AppointmentTest.java
@@ -1,1 +1,19 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
+public class AppointmentTest {
+  private Appointment appointment;
+  private Appointment appointment2;
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "matite", "kevmatite7862");
+    appointment = new Appointment("2017-10-19 10:00:00", 1, 1);
+    appointment2 = new Appointment("2017-01-19 9:00:00", 1, 1);
+  }
+  
+  @Test
+  public void Appointment_instantiates_true() {
+    assertEquals(true, appointment instanceof Appointment);
+  }
