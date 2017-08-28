1cf60d8372b16f752f2c625b9f6b719b671eb298
kev7862 <kevmatite7862@gmail.com>
2017-08-28 16:44:38 +0300

    set-up Heroku port by adding additional logic in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,20 @@ import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-
 public class App {
   public static void main(String[] args) {
+
+// Additional logic for heroku deployment.
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
