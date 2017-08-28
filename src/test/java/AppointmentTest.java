011046f6abdef7193dc6c3570863fb9a88653647
kev7862 <kevmatite7862@gmail.com>
2017-08-28 08:29:54 +0300

    Edited my database password in AppointmentTest.java

--- src/test/java/AppointmentTest.java
+++ src/test/java/AppointmentTest.java
@@ -8,11 +8,11 @@ public class AppointmentTest {
 
   @Before
   public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "matite", "kevmatite7862");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "matite", "Phlipper7862");
     appointment = new Appointment("2017-10-19 10:00:00", 1, 1);
     appointment2 = new Appointment("2017-01-19 9:00:00", 1, 1);
   }
-  
+
   @Test
   public void Appointment_instantiates_true() {
     assertEquals(true, appointment instanceof Appointment);
